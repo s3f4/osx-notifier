070686b5f3fe6e4cd7e519246d9913cc368fd43d
s3f4 <sefa@dehaa.com>
2019-01-08 23:34:41 +0300

    interval added

--- execute.py
+++ execute.py
@@ -1,11 +1,14 @@
 from exchange import Exchange
 import ccxt, time
 from util import Util
+import traceback
 
 
 class Execute(object):
     exchange = None
     config = None
+    minute = 0
+    trigger = False
 
     def __init__(self):
         """
@@ -16,13 +19,6 @@ class Execute(object):
                                  self.config["binance_api_key"],
                                  self.config["binance_api_secret"])
 
-    def follow_notify(self):
-        if self.config["follow"] is not None:
-            for follow in self.config["follow"]:
-                tick = self.exchange.fetch_ticker(follow)["close"]
-                Util.notify(title=follow, subtitle="%.08f" % tick, message="")
-                time.sleep(4)
-
     def balances_and_btc(self):
         """
 
@@ -31,16 +27,40 @@ class Execute(object):
         return self.exchange.fetch_balance()["info"]["balances"], float(
             self.exchange.fetch_ticker("BTC/" + self.config["stable"])["close"])
 
+    def notify(self, type, title=None, subtitle=None, message=None):
+        """
+
+        :param type:
+        :param title:
+        :param subtitle:
+        :param message:
+        """
+        if self.trigger:
+            if type == "follow":
+                if self.config["follow"] is not None:
+                    for follow in self.config["follow"]:
+                        tick = self.exchange.fetch_ticker(follow)["close"]
+                        Util.notify(title=follow, subtitle="%.08f" % tick, message="")
+                        time.sleep(4)
+            elif type == "asset" or type == "general":
+                Util.notify(title, subtitle, message)
+
+    def set_trigger(self):
+        """
+        """
+        self.trigger = self.minute == 0 or \
+                       self.config["interval"] is not None and \
+                       self.minute % self.config["interval"] == 0
+
     def notification_loop(self):
         """
 
         """
-        minute = 0
         currency = 0
 
         while True:
-
-            self.follow_notify()
+            self.set_trigger()
+            self.notify(type="follow")
             balances, BTC_Stable = self.balances_and_btc()
             TotalBTCBalance = 0.0
 
@@ -55,7 +75,8 @@ class Execute(object):
                         BTCBalance = Asset_BTC * amount
                         TotalBTCBalance += BTCBalance
                         if BTCBalance >= 0.01:
-                            Util.notify(title=balance["asset"], subtitle="Amount : {}".format("%.4f" % amount),
+                            self.notify(type="asset", title=balance["asset"],
+                                        subtitle="Amount : {}".format("%.4f" % amount),
                                         message=balance["asset"] + "/BTC : {}".format("%.8f" % Asset_BTC))
                             time.sleep(4)
                     except ccxt.errors.ExchangeError:
@@ -65,21 +86,22 @@ class Execute(object):
 
             USDBalance = TotalBTCBalance * BTC_Stable
 
-            if currency == 0 or minute % 30 == 0:
+            if currency == 0 or self.minute % 30 == 0:
                 currency = Util.currency(self.config["currency"])
 
-            Util.notify(title='BTC : {} - {}'.format("%.4f" % TotalBTCBalance, "%.2f" % BTC_Stable),
+            self.notify(type="general", title='BTC : {} - {}'.format("%.4f" % TotalBTCBalance, "%.2f" % BTC_Stable),
                         subtitle='USD : {}'.format("%.4f" % USDBalance),
                         message='`echo TRY : {} &&'.format(
                             "%.4f" % (USDBalance * currency)) + ' echo USD/TRY: ' + str(currency) + '`')
 
             time.sleep(60)
-            minute += 1
+            self.minute += 1
 
 
 if __name__ == '__main__':
     try:
         execute = Execute()
         execute.notification_loop()
-    except:
-        print("An error occured")
+    except Exception as e:
+        print(str(e))
+        traceback.print_exc()
